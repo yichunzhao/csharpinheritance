c3f970ec6c728402ef0e3c796b368a14e4bddc11
yichunzhao <zhaoyichun@gmail.com>
2022-04-04 02:12:40 +0200

    Initial commit

---
+++ csharpinheritance/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace csharpinheritance
+{
+    abstract class Vehicle
+    {
+        protected string brand;
+
+        protected Vehicle(String brand)
+        {
+            this.brand = brand;
+        }
+
+        public String getBrand()
+        {
+            return this.brand;
+        }
+
+        public void honk()
+        {
+            Console.WriteLine("Vehicle honk!");
+        }
+        
+    }
+
+    class Car : Vehicle
+    {
+        
+        public Car(string brand) : base(brand)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Ford");
+            car.honk();
+            Console.WriteLine("Car brand: "+ car.getBrand());
+        }
+    }
+}
